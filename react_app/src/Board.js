6e0894c39dcdb77917ccacf4c2e5f31f26df6214
Ritesh Kumar 7 <rku202@sapient.com>
2018-04-03 17:14:11 +0530

    app

--- react_app/src/Board.js
+++ react_app/src/Board.js
@@ -1,83 +1,145 @@
-import React, {Component} from 'react'
-import Note from './Note'
-import FaPlus from 'react-icons/lib/fa/plus'
-
-class Board extends Component {
-
-     constructor(props){
-         super(props)
-         this.state={
-             notes:  []
-         }
-         this.add= this.add.bind(this)
-         this.eachNote= this.eachNote.bind(this)
-         this.update= this.update.bind(this)
-         this.remove= this.remove.bind(this)
-         this.nextId= this.nextId.bind(this)
-     }
-
-     componentWillMount(){
-         var self= this
-         if(this.props.count){
-             fetch(`https://baconipsum.com/api/?type=all-meat&sentences=${this.props.count}`)
-             .then(response=> response.json())
-             .then(json=> json[0].split('. ')
-             .forEach(sentence=> self.add(sentence.substring(0,25))))
-         }
-     }
-
-     add(text){
-         this.setState(prevState=>({
-             notes: [...prevState.notes,
-                 {
-                     id: this.nextId(),
-                     note: text
-                 }
-            ]
-         }))
-     }
-
-     nextId(){
-         this.uniqueId= this.uniqueId || 0
-         return this.uniqueId++
-     }
-
-     update(newText,i){
-         console.log('updating item at ',i,newText)
-         this.setState(prevState=>({
-              notes:prevState.notes.map(
-                  note=> (note.id!==i)? note : {...note, note: newText}
-              )
-         }))
-     }
-
-     remove(id){
-         console.log('removing item at ',id)
-         this.setState(prevState=>({
-             notes: prevState.notes.filter( note=> (note.id!==id) )
-         }))
-     }
-
-     eachNote(note,i){
-         return (
-             <Note key={note.id} index= {note.id} 
-             onChange={this.update}
-             onRemove={this.remove}
-             >{note.note}</Note>
-         )
-     }
-
-    render () {
-        return(
-             <div className="board">
-             {this.state.notes.map(this.eachNote)}
-             <button onClick= {this.add.bind(null,"New Note")}
-             id="add">
-             <FaPlus />
-             </button>
-             </div>
-        )
+function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
+  
+  class Board extends React.Component {
+    renderSquare(i) {
+      return (
+        <Square
+          value={this.props.squares[i]}
+          onClick={() => this.props.onClick(i)}
+        />
+      );
     }
-}
-
-export default Board 
+  
+    render() {
+      return (
+        <div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  class Game extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        history: [
+          {
+            squares: Array(9).fill(null)
+          }
+        ],
+        stepNumber: 0,
+        xIsNext: true
+      };
+    }
+  
+    handleClick(i) {
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
+      const current = history[history.length - 1];
+      const squares = current.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext ? "X" : "O";
+      this.setState({
+        history: history.concat([
+          {
+            squares: squares
+          }
+        ]),
+        stepNumber: history.length,
+        xIsNext: !this.state.xIsNext
+      });
+    }
+  
+    jumpTo(step) {
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0
+      });
+    }
+  
+    render() {
+      const history = this.state.history;
+      const current = history[this.state.stepNumber];
+      const winner = calculateWinner(current.squares);
+  
+      const moves = history.map((step, move) => {
+        const desc = move ?
+          'Go to move #' + move :
+          'Go to game start';
+        return (
+          <li key={move}>
+            <button onClick={() => this.jumpTo(move)}>{desc}</button> 
+          </li>
+        );
+      });
+  
+      let status;
+      if (winner) {
+        status = "Winner: " + winner;
+      } else {
+        status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+      }
+  
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board
+              squares={current.squares}
+              onClick={i => this.handleClick(i)}
+            />
+          </div>
+          <div className="game-info">
+            <div>{status}</div>
+            <ol>{moves}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  // ========================================
+  
+  ReactDOM.render(<Game />, document.getElementById("root"));
+  
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+  
